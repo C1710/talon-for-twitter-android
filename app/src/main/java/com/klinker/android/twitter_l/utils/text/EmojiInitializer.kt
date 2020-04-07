f669ac0760a9eecb35da71cd92fc1521e0c9bd69
Constantin A <git-user@c1710.de>
2020-04-07 21:12:55 +0200

    Update to the latest upstream version & FileMojiCompat

--- app/src/main/java/com/klinker/android/twitter_l/utils/text/EmojiInitializer.kt
+++ app/src/main/java/com/klinker/android/twitter_l/utils/text/EmojiInitializer.kt
@@ -3,8 +3,6 @@ package com.klinker.android.twitter_l.utils.text
 import android.content.Context
 import de.c1710.filemojicompat.FileEmojiCompatConfig
 import androidx.emoji.text.EmojiCompat
-import androidx.emoji.text.FontRequestEmojiCompatConfig
-import androidx.core.provider.FontRequest
 
 object EmojiInitializer {
 
